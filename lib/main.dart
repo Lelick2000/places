8da8f65272b4701cde6975a4da373edfcb6cb7b7
Lelick <amitkov@playershealth.com>
2022-07-13 10:48:25 +0300

    fix lint

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,13 @@ void main() {
 class MyFirstWidget extends StatelessWidget {
   int _counter = 0;
 
+  MyFirstWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     _counter++;
-    print("Counter = $_counter");
+    print('Counter = $_counter');
+
     return Container(
       child: Center(
         child: Text('Hello!'),
