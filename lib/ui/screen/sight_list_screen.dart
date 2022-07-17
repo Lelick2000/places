803da6623390596e3c6530c8278e98548cf74c30
Lelick <amitkov@playershealth.com>
2022-07-17 17:14:29 +0300

    task 4-4 ready

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,8 +10,58 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Hello!')),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: const Center(child: Text('Hello!')),
+      appBar: AppBar(
+        toolbarHeight: 136,
+        titleSpacing: 16,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: /*const Text(
+          'Список\nинтересных мест',
+          maxLines: 2,
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+          ),
+        )*/
+            RichText(
+          maxLines: 2,
+          text: const TextSpan(
+            text: 'С',
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.green,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w700,
+            ),
+            children: [
+              TextSpan(
+                text: 'писок\n',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              TextSpan(
+                text: 'и',
+                style: TextStyle(
+                  color: Colors.yellow,
+                ),
+              ),
+              TextSpan(
+                text: 'нтересных мест',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
